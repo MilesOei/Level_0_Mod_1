1ae4f7f48a3345996f12ee8803b6056ef99c12e4
MilesOei <30487851+MilesOei@users.noreply.github.com>
2017-08-09 14:29:51 -0700

    Add files via upload

--- src/variables_and_conditionals/ChangeCalculator.java
+++ src/variables_and_conditionals/ChangeCalculator.java
@@ -17,16 +17,22 @@ public class ChangeCalculator {
     public static void main(String[] args) {
 
         // Ask the user how many nickels they have
-
+String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
         // Convert their answer to an int using Integer.parseInt()
-
+int numNickels = Integer.parseInt(nickels);
         // Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+int numDimes = Integer.parseInt(dimes);
         // Ask the user how many quarters they have, and convert their answer
-
+String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+int numQuarters = Integer.parseInt(quarters);
         // Calculate how much money the user has and save it in a double variable 
+double totalMoney = 0;
+totalMoney+=(.25 * numQuarters);
+totalMoney+=(.10 * numDimes);
+totalMoney+=(.05 * numNickels);
 
         // Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "You have $" + totalMoney);
     }
 }
--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,22 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
+	String Miles = "Teleporting";
+	String Matt = "Breating underwater";
+	String Mario = "Parcour";
         // 2. Ask the user to enter a name. Store their answer in a variable.
-
+	String name = JOptionPane.showInputDialog("Please enter your name.");
         // 3. Show the superpower in a pop-up, depending on the name entered. 
-
+	if(name.equalsIgnoreCase("Miles")) {
+		JOptionPane.showMessageDialog(null, name + "'s superpower is..." + Miles);		
+	} else if(name.equalsIgnoreCase("Matt")) {
+		JOptionPane.showMessageDialog(null, name + "'s superpower is..." + Matt);
+	} else if(name.equalsIgnoreCase("Mario")) {
+		JOptionPane.showMessageDialog(null, name + "'s superpower is..." + Mario);
+	} else {
+		JOptionPane.showMessageDialog(null, "The name you entered is not listed.");
+		
+	}
+	
     }
 }
