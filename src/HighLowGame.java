b6c7435075a301f634442799f331318dbcae9e46
League <league@WTS5.attlocal.net>
2017-08-11 14:27:03 -0700

    Summer Slam Day 5

---
+++ src/HighLowGame.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+// Copyright Wintriss Technical Schools 2013
+public class HighLowGame {
+
+	public static void main(String[] args) {
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(101);
+		// 2. Print out the random variable above
+		// System.out.println(random);
+		// 11. do the following 10 times
+		for (int i = 0; i < 10; i++) {
+
+			// 1. ask the user for a guess using a pop-up window, and save their
+			// response
+			String guess = JOptionPane.showInputDialog("Take a guess");
+			// 4. convert the users’ answer to an int (Integer.parseInt(string))
+			int guessInt = Integer.parseInt(guess);
+			// 5. if the guess is correct
+			if (guessInt == random) {
+				JOptionPane.showMessageDialog(null, "You won!!!");
+			} else if (guessInt > random) {
+				JOptionPane.showMessageDialog(null, "Your guess is too high!");
+			} else if (guessInt < random) {
+				JOptionPane.showMessageDialog(null, "Your guess is too low!");
+			}
+
+		}
+		// 6. win
+		// 7. if the guess is high
+		// 8. tell them it's too high
+		// 9. if the guess is low
+		// 10. tell them it's too low
+
+		// 12. tell them they lose
+		JOptionPane.showMessageDialog(null, "You lose try again");
+
+	}
+
+}
--- src/for_loops/ForLoopGauntlet.java
+++ src/for_loops/ForLoopGauntlet.java
@@ -2,16 +2,13 @@ package for_loops;
 
 public class ForLoopGauntlet {
 	public static void main(String[] args) {
-	for (int i = 1; i <= 99; i+=2) {
-		System.out.println(i);
-	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+		for (int i = 1; i < 100; i += 10) {
+			for (int j = i; j < i + 10; j++) {
+				System.out.print(j + "\t");
+			}
+			System.out.println();
+		}
+
 	}
 }
